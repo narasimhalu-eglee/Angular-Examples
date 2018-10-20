95b3cba97c66fe7d98d4a221d9e122f12519ca83
Narasimhalu Jeglee <naraj.ise@gmail.com>
2018-10-20 13:03:36 +0530

    Created Componenets

--- MyFirstAngularApp/src/app/app.module.ts
+++ MyFirstAngularApp/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DetailsComponent } from './details/details.component';
+import { PostsComponent } from './posts/posts.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    DetailsComponent,
+    PostsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
